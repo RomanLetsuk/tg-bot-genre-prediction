020f2ab91a8b34c00e213b998eb1b452466b9077
Roman Letsuk <romaletsuk@gmail.com>
2020-12-27 18:25:19 +0300

    updated url

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1330439927:AAEoIpXtlgVVXNDtORszMnwzBI9DOHyeWV4"
 bot_user_name = "rl_genre_prediction_bot"
-URL = "https://tg-bot-genre-prediction.herokuapp.com"
+URL = "https://tg-bot-genre-prediction.herokuapp.com/"
